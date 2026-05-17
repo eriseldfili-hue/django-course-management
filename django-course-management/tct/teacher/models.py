1bc5d37735c3e284cbb9dabd7674af1c743778cf
Eriseld Fili <eriseld.fili@gmail.com>
2026-05-17 14:50:17 +0200

    Refactor courses structure into packages (models, serializers, data_service) and add enrollment module

--- django-course-management/tct/teacher/models.py
+++ django-course-management/tct/teacher/models.py
@@ -3,10 +3,9 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    course = models.ForeignKey(
-        'Course',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='assigned_teachers'
-    )
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
